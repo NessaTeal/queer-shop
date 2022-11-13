d2c535df2c4f41ea59e0f203c0379a2b8d294b79
Nessa Teal <nessa.s.teal@gmail.com>
2022-11-13 18:37:48 +0200

    Remove import

--- src/flag/Flag.tsx
+++ src/flag/Flag.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Group, Rect } from 'react-konva';
 import Konva from 'konva';
 import { FlagDefinition } from './flag-definitions';
